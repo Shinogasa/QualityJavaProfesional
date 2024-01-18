78675eb794bf494ee7c4240674247e45efae0ba5
Shinonome <cuulbulu@gmail.com>
2024-01-18 23:09:08 +0900

    Update: 9章まで

---
+++ JavaForPro/src/main/java/projava/LoopStepSample.java
@@ -0,0 +1,10 @@
+package projava;
+
+public class LoopStepSample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+           System.out.print("0".repeat(i));
+            System.out.println();
+        }
+    }
+}
