e5e8300a3d485ce50b749604153f509794d1ec92
Shinonome <cuulbulu@gmail.com>
2024-01-20 15:30:02 +0900

    Update: 9章まで

---
+++ JavaForPro/src/main/java/projava/StreamSample1.java
@@ -0,0 +1,15 @@
+package projava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamSample1 {
+    public static void main(String[] args) {
+        var data = List.of("yamamopto", "kis", "sugiyama");
+
+        var result = data.stream().filter(s -> s.length() >= 5).collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println(result);
+    }
+}
