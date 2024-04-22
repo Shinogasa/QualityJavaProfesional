58dd01d750a1adcf1aed52ff9d10983513a3ea5b
Shinonome <cuulbulu@gmail.com>
2024-04-22 10:30:48 +0900

    台4部おわり

---
+++ JavaForPro/src/main/java/projava/ParamSample.java
@@ -0,0 +1,14 @@
+package projava;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ParamSample {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
+
+class Dummy {
+
+}
