8d0b36fbe832323d6fb1bc44e396a3d58325d626
Shinonome <cuulbulu@gmail.com>
2024-05-06 15:07:38 +0900

    21章

---
+++ tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -0,0 +1,39 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/hello")
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
+    }
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/list")
+    String listItems(Model model) {
+        model.addAttribute("taskList", taskItems);
+        return "home";
+    }
+
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "redirect:/list";
+    }
+}
